caa56c4c228cf487af48ba2a077988aa511fe053
bkapustik <82807109+bkapustik@users.noreply.github.com>
2024-06-18 18:21:07 +0200

    feat(CalendarComponent): Initial project creation

---
+++ src/Kentico.Xperience.CalendarComponent/Components/FormComponents/CalendarFormComponent.cs
@@ -0,0 +1,25 @@
+﻿using Kentico.Forms.Web.Mvc;
+using Kentico.Xperience.CalendarComponent.Components.FormComponents;
+
+[assembly: RegisterFormComponent(
+    identifier: CalendarFormComponent.IDENTIFIER,
+    formComponentType: typeof(CalendarFormComponent),
+    name: "{$kentico.xperience.formbuilder.component.formcomponent.name$}",
+    Description = "{$kentico.xperience.formbuilder.component.formcomponent.description$}",
+    IconClass = "",
+    ViewName = "~/Components/FormComponents/CalendarFormComponent.cshtml")]
+
+namespace Kentico.Xperience.CalendarComponent.Components.FormComponents;
+
+public class CalendarFormComponent : FormComponent<CalendarFormComponentProperties, DateOnly>
+{
+    public const string IDENTIFIER = nameof(CalendarFormComponent);
+
+    public DateOnly Date { get; set; }
+
+    public override bool CustomAutopostHandling => true;
+
+    public override DateOnly GetValue() => Date;
+
+    public override void SetValue(DateOnly value) => Date = value;
+}
---
+++ src/Kentico.Xperience.CalendarComponent/Components/FormComponents/CalendarFormComponentProperties.cs
@@ -0,0 +1,15 @@
+﻿using CMS.DataEngine;
+
+using Kentico.Forms.Web.Mvc;
+
+namespace Kentico.Xperience.CalendarComponent.Components.FormComponents;
+
+public class CalendarFormComponentProperties : FormComponentProperties<DateOnly>
+{
+    public CalendarFormComponentProperties() : base(FieldDataType.Date)
+    {
+    }
+
+    [DefaultValueEditingComponent(default)]
+    public override DateOnly DefaultValue { get; set; }
+}
